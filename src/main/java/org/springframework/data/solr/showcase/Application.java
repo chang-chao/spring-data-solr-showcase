88b128d7ffc6406b5ff64c768d2fbde62eae9371
chang chao <chang-chao@hotmail.com>
2017-06-22 14:45:32 +0900

    configuration

--- src/main/java/org/springframework/data/solr/showcase/Application.java
+++ src/main/java/org/springframework/data/solr/showcase/Application.java
@@ -17,6 +17,9 @@ package org.springframework.data.solr.showcase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author Christoph Strobl
@@ -28,4 +31,13 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer mvcViewConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addViewControllers(ViewControllerRegistry registry) {
+				registry.addViewController("/").setViewName("home");
+			}
+		};
+	}
 }
